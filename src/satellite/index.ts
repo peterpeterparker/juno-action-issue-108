39e7af92b0a4e52339d00ed0bf6cde0763c66d84
David Dal Busco <david.dalbusco@outlook.com>
2026-06-05 15:22:02 +0200

    test: no query

--- src/satellite/index.ts
+++ src/satellite/index.ts
@@ -1,4 +1,4 @@
-import { defineQuery } from "@junobuild/functions";
+import { defineQuery, defineHook, type OnSetDoc } from "@junobuild/functions";
 import { j } from "@junobuild/schema";
 import { canisterSelf } from "@junobuild/functions/ic-cdk";
 
@@ -6,11 +6,17 @@ const Schema = j.strictObject({
   id: j.principal(),
 });
 
-export const helloWorld = defineQuery({
-  result: Schema,
-  handler: async () => {
-    return {
-      id: canisterSelf()
-    }
-  },
-});
+export const onSetDoc = defineHook<OnSetDoc>({
+  collections: ["demo"],
+  run: async (context) => {
+    console.log(context)
+  }});
+
+// export const helloWorld = defineQuery({
+//   result: Schema,
+//   handler: async () => {
+//     return {
+//       id: canisterSelf()
+//     }
+//   },
+// });
